59feab6f750810d3f57ff34d3fcab3aeaf369e62
PauloMatiasxvs <155742553+PauloMatiasxvs@users.noreply.github.com>
2025-02-21 10:42:58 -0300

    commit

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://user:password@localhost/projetoMED"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "supersecretkey"
--- frontend/src/App.js
+++
@@ -1,2 +0,0 @@
-import Login from "./pages/Login";
-
